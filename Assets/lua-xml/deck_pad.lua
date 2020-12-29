099de0414405fc3920fe394d1d730321ab735a6d
Tom <tomsteventon@gmail.com>
2020-12-29 04:03:23 +0000

    Change castle origins and updated deck_pad lua

--- Assets/lua-xml/deck_pad.lua
+++ Assets/lua-xml/deck_pad.lua
@@ -1,14 +1,56 @@
 function onLoad()
+    --[[
+        This script doesn't do much except draw some buttons and call the checkDeck function in Global
+    --]]
     deckLock = false
     deckObj  = false
-    player   = "blue"
-    if self.getGUID()~="blu007" then player = "red" end
-
+    owner    = "blue"
+    if self.getGUID()~="blu007" then owner = "red" end
     drawButtons()
 end
 
+function checkDeck(obj, clickee)
+    --[[
+        Main function. Called whenever a user clicks the onboard "Lock" button. 
+        Checks for a deck object sitting on its face (heh) and calls the checkDeck function in Global
+        to see if it's a legit deck. If it passes, Global will trigger the lockDeck function on this object.
+    --]]
+
+    --simple flag
+    local foundDeck = false
+
+    --set cast params
+    local size    = self.getBounds().size
+          size.y  = 4
+    local objs    = Physics.cast({
+        origin    = self.getPosition() + vector(0, size.y/2, 0),
+        type      = 3,
+        size      = size,
+        direction = {0, 1, 0}
+    })
+
+    --check cast objects for first Deck found and pass it to global
+    for k,obj in ipairs(objs) do
+        if obj.hit_object.tag=="Deck" then
+            Global.call("checkDeck", {owner = owner, obj = obj.hit_object})
+            log("Passed deck "..obj.hit_object.getGUID().." to Global checkDeck()")
+            foundDeck = true
+            break
+        end
+    end
+
+    --warn user if no deck found
+    if not foundDeck then 
+        broadcastToColor("You must place a deck in the zone to check", clickee, "Red")
+    end
+end
 
 function lockDeck(t)
+    --[[
+        Called by Global when the deck has passed the checkDeck function
+        Sets deckLock to true and updates the deckObj ref to whatever global said it should be
+    --]]
+
     if deckLock then return false end
     log("Deck locked")
     deckLock = true
@@ -17,17 +59,27 @@ function lockDeck(t)
 end
 
 function unlockDeck()
+    --[[
+        Called by either Global OR by a player clicking the onboard button
+        Sets deckLock to true and updates the deckObj ref to whatever global said it should be
+        Calls global either way to make sure the otehr game important stuff happens
+    --]]
     if not deckLock then return false end
-    log("Deck unlocked")
-    deckLock = false
     if deckObj then
-        deckObj.setLock(false)
-        deckObj.interactable = true
+        Global.call("unlockDeck", {
+            owner = owner:lower()
+        })
+        deckLock = false
+        drawButtons()
+    else
+        print("Can't unlock deck for owner "..owner..", no deckObj registered")
     end
-    drawButtons()
 end
 
 function drawButtons()
+    --[[
+        Draws the two buttons for Locking and unLocking.
+    --]]
     self.clearButtons()
     local btn = {
         function_owner = self,
@@ -51,31 +103,4 @@ function drawButtons()
     btn.position[1]    = 0.6
     btn.font_color, btn.color = btn.color, btn.font_color
     self.createButton(btn)
-end
-
-
-
-function checkDeck(obj, player_color)
-    local foundDeck = false
-    local size    = self.getBounds().size
-          size.y  = 4
-    local objs    = Physics.cast({
-        origin    = self.getPosition() + vector(0, size.y/2, 0),
-        type      = 3,
-        size      = size,
-        direction = {0, 1, 0},
-        debug     = true
-    })
-
-    for k,obj in ipairs(objs) do
-        if obj.hit_object.tag=="Deck" then
-            Global.call("checkDeck", {player = player, obj = obj.hit_object})
-            log("Passed deck "..obj.hit_object.getGUID().." to Global checkDeck()")
-            foundDeck = true
-            break
-        end
-    end
-    if not foundDeck then 
-        broadcastToColor("You must place a deck in the zone to check", player_color, "Red")
-    end
 end
