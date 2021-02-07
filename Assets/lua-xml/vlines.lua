34a43e19b6ad50dd58cf4a7f3043418e6139e17c
Tom <tomsteventon@gmail.com>
2021-02-07 00:24:39 +0000

    Fixed vline position

--- Assets/lua-xml/vlines.lua
+++ Assets/lua-xml/vlines.lua
@@ -4,14 +4,14 @@ function outlinePlayerHands()
 
     for k,v in ipairs(Hands.getHands()) do
         local rot = v.getRotation().y
-        local pos = v.getPosition():setAt("y", 1)
+        local pos = v.getPosition():setAt("y", 0.6)
         local color = Color.fromString(v.getValue())
-        local scale = v.getScale()
-        local scale2 = scale:copy():scale(Vector(1.05, 1, 1.25))
+        local scale = v.getScale():scale(Vector(0.7, 1, 0.8))
+        local scale3 = scale:copy():scale(Vector(1.025, 1, 1.175))
 
 
         table.insert(vlines, vline_capsule(pos, rot, scale, color, 0.25))
-        table.insert(vlines, vline_capsule(pos, rot, scale2, color, 0.1))
+        table.insert(vlines, vline_capsule(pos, rot, scale3, color, 0.1))
     end
 
     Global.setVectorLines(vlines)
